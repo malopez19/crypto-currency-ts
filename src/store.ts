17c2eac93edac69769da4d07f146c2f1065269de
mjuradol <lopezmateojurado@gmail.com>
2024-11-07 20:27:22 -0500

    add services folder and created store service

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,11 @@
-import { create }from 'zustand'
-import axios from 'axios'
+import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
-import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema'
 import { Cryptocurrency } from './types'
+import { getCryptos } from './services/CryptoServices'
 
 type CryptoStore = {
     cryptocurrencies: Cryptocurrency[]
     fetchCryptos: () => Promise<void>
-}
-async function getCryptos() {
-    const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`
-    const { data: {Data} } = await axios.get(url)
-    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-
-    if(result.success){
-        return result.data
-    }
 }
 
 export const useCrytoStore = create<CryptoStore>()(devtools((set) => ({
