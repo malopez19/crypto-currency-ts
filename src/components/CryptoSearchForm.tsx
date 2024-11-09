f83a671dcc89b895b5bf25caca8c2841f12df174
mjuradol <lopezmateojurado@gmail.com>
2024-11-08 19:16:38 -0500

    create state currenccy

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -1,17 +1,50 @@
 import { useCrytoStore } from '../store'
 import { currencies } from "../data"
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { Pair } from '../types'
+import ErrorMessage from './ErrorMessage'
 
 export default function CryptoSearchForm() {
 
     const cryptocurrencies = useCrytoStore((state) => state.cryptocurrencies)
+    const [pair, setPair] = useState<Pair>({
+        currency: '',
+        cryptocurrency: ''
+    })
+    const [error, setError] = useState('')
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setPair({
+            ...pair,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(Object.values(pair).includes('')){
+            setError('Todos los campos son obligatorios')
+            return 
+        }
+        setError('')
+        //consultar API
+
+
+    }
 
     return (
-        <form className="form">
+        <form 
+            className="form"
+            onSubmit={handleSubmit}
+        >
+            {error && <ErrorMessage>{error}</ErrorMessage>}
             <div className="field">
                 <label htmlFor="currency">Moneda:</label>
                 <select
                     name="currency"
                     id="currency"
+                    onChange={handleChange}
+                    value={pair.currency}
                 >
                     <option value=""> -- Seleccione --</option>
                     {currencies.map((currency) => (
@@ -21,10 +54,12 @@ export default function CryptoSearchForm() {
             </div>
             
             <div className="field">
-                <label htmlFor="criptocurrency">Criptomoneda:</label>
+                <label htmlFor="cryptocurrency">Criptomoneda:</label>
                 <select
-                    name="criptocurrency"
+                    name="cryptocurrency"
                     id="criptocurrency"
+                    onChange={handleChange}
+                    value={pair.cryptocurrency}
                 >
                     <option value=""> -- Seleccione --</option>
                     {cryptocurrencies.map((crypto) => (
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
-import { currencySchema, CryptoCurrencyResponseSchema } from '../schema/crypto-schema'
+import { currencySchema, CryptoCurrencyResponseSchema, PairSchema } from '../schema/crypto-schema'
 
 export type Currency = z.infer<typeof currencySchema>
 export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema>
+export type Pair = z.infer<typeof PairSchema>
+
